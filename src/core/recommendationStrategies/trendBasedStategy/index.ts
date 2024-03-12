a27b9ef7aa5c39b65068308bd179cdd452c1e975
Brian Miranda <brianmiranda.dev@gmail.com>
2024-03-12 15:49:23 -0400

    :hammer: modify trendBasedStrategy algorythm to accept configurations as arguments

--- src/core/recommendationStrategies/trendBasedStategy/index.ts
+++ src/core/recommendationStrategies/trendBasedStategy/index.ts
@@ -1,5 +1,9 @@
-import { Stocks } from '../../global/types/stocks.types';
+import { Stocks } from '../../../global/types/stocks.types';
 import { Recommendation } from './types';
+import {
+  SocialMediaKeys,
+  TrendBasedConfig,
+} from '../../../global/types/strategiesConfigs.types';
 
 const PRICE_CHANGE_THRESHOLD_BUY = 0.05;
 const PRICE_CHANGE_THRESHOLD_SELL = -0.05;
@@ -12,15 +16,34 @@ function calculatePriceChange(stocks: Stocks): number {
   return (finalPrice - initialPrice) / initialPrice;
 }
 
-function calculateSocialMediaTrend(stocks: Stocks): number {
-  const initialCount: number = stocks[0].socialMediaCount;
-  const finalCount: number = stocks[stocks.length - 1].socialMediaCount;
-  return (finalCount - initialCount) / initialCount;
+function calculateSocialMediaTrend(
+  stocks: Stocks,
+  socialMediaServices: Array<SocialMediaKeys>
+): number {
+  let initialTotalCount: number = 0;
+  let finalTotalCount: number = 0;
+
+  socialMediaServices.forEach((service) => {
+    const initialCount: number = stocks[0].socialMediaCount[service];
+    const finalCount: number =
+      stocks[stocks.length - 1].socialMediaCount[service];
+
+    initialTotalCount += initialCount;
+    finalTotalCount += finalCount;
+  });
+
+  return (finalTotalCount - initialTotalCount) / initialTotalCount;
 }
 
-function trendBasedStrategy(stocks: Stocks): Recommendation {
+function trendBasedStrategy(
+  stocks: Stocks,
+  config: TrendBasedConfig
+): Recommendation {
   const priceChange: number = calculatePriceChange(stocks);
-  const socialMediaTrend: number = calculateSocialMediaTrend(stocks);
+  const socialMediaTrend: number = calculateSocialMediaTrend(
+    stocks,
+    config.socialMediaServices
+  );
 
   if (
     priceChange > PRICE_CHANGE_THRESHOLD_BUY &&
