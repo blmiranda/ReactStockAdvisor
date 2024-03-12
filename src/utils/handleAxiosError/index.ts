76bea932e8c9df4faff9cf81475ce2223e3f53f1
Brian Miranda <brianmiranda.dev@gmail.com>
2024-03-12 11:24:13 -0400

    :sparkles: create utility function to handle axios errors

---
+++ src/utils/handleAxiosError/index.ts
@@ -0,0 +1,21 @@
+import { AxiosError } from 'axios';
+
+function handleAxiosError(error: AxiosError): Error {
+  if (error.response) {
+    console.error(error.response.data);
+    console.error(error.response.status);
+    console.error(error.response.headers);
+
+    return new Error('Response error');
+  }
+
+  if (error.request) {
+    console.error(error.request);
+    return new Error('Unable to connect to the server.');
+  }
+
+  console.log(error.config);
+  return new Error('There was a problem requesting your request');
+}
+
+export default handleAxiosError;
