b043a226e5e5185d66501f85feacbd1f539f5392
Brian Miranda <brianmiranda.dev@gmail.com>
2024-03-13 13:38:47 -0400

    :sparkles: create icons map for the stocks

---
+++ src/utils/stockIconsMap/index.ts
@@ -0,0 +1,13 @@
+import { IconsMap } from './types';
+
+const stockIconsMap: IconsMap = {
+  AAPL: ['fab', 'apple'],
+  MSFT: ['fab', 'apple'],
+  GOOGL: ['fab', 'apple'],
+  META: ['fab', 'apple'],
+  ABNB: ['fab', 'apple'],
+  AMZN: ['fab', 'apple'],
+  UBER: ['fab', 'apple'],
+};
+
+export default stockIconsMap;
