a4f13e84bb5b1743e84ffca7cf4a50b6254e0a3a
Brian Miranda <brianmiranda.dev@gmail.com>
2024-03-13 13:49:02 -0400

    :lipstick: create styles for the options form component

---
+++ src/components/OptionsForm/styles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+import theme from '../../global/styles/theme';
+import { translateY } from './animations.ts';
+
+export default StyleSheet.create({
+  buttonsContainer: {
+    paddingVertical: 12,
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 6,
+  },
+
+  divider: {
+    backgroundColor: theme.colors.base100,
+    height: 1.5,
+  },
+
+  buttonOptionsContainer: {
+    overflow: 'hidden',
+  },
+
+  buttonOptions: {
+    height: 60,
+
+    transform: [{ translateY: translateY }],
+  },
+
+  flatList: {
+    gap: 10,
+
+    paddingHorizontal: 28,
+    paddingVertical: 12,
+  },
+});
