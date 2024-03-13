b350a1aa762f146da69bf93cdb1f976deb6e1ec3
Brian Miranda <brianmiranda.dev@gmail.com>
2024-03-13 13:37:59 -0400

    :sparkles: create social media options file

---
+++ src/utils/socialMediaOptions/index.ts
@@ -0,0 +1,26 @@
+import { SocialMediaOptions } from '../../global/types/socialMediaOptions.types';
+
+const socialMediaOptions: SocialMediaOptions = [
+  {
+    name: 'Facebook',
+    value: 'facebook',
+    icon: 'facebook',
+  },
+  {
+    name: 'Instagram',
+    value: 'instagram',
+    icon: 'instagram',
+  },
+  {
+    name: 'Twitter',
+    value: 'twitter',
+    icon: 'twitter',
+  },
+  {
+    name: 'Tiktok',
+    value: 'tiktok',
+    icon: 'tiktok',
+  },
+];
+
+export default socialMediaOptions;
